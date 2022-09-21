ef39da192805b1cc7008c134e756a4ce5b194791
Eko J. Salim <eko@ekojs.com>
2022-09-22 05:45:18 +0700

    Rewrite BIC to avoid recursion and use bitarray

--- compression.py
+++ compression.py
@@ -1,6 +1,8 @@
 import array
 import math
 from bitstring import BitStream, BitString, pack
+from bitarray import bitarray
+from bitarray.util import int2ba, ba2int
 
 
 class StandardPostings:
@@ -62,83 +64,95 @@ class BICPostings:
         """
         Get bit representation of x using ceil(log2(r + 1)) bits
         """
-        b = math.ceil(math.log2(r + 1))
-        b = max(b, 1)
-        return pack(f"uint:{b}={x}")
-
-    @staticmethod
-    def next_r_bits_to_int(stream: BitStream, r):
-        b = math.ceil(math.log2(r + 1))
-        b = max(b, 1)
-        x = stream.read(f"uint:{b}")
-        return x
+        b = len(int2ba(r))
+        return int2ba(x, b)
 
     @staticmethod
     def bic_encode(s, n, lo, hi):
-        assert lo <= hi
-        out = BitStream()
-        m = n // 2
-        x = s[m]
-        out.append(BICPostings.bit_representation(x - lo - m, hi - lo - n + 1))
-        if (n1 := m) > 0:
-            out.append(BICPostings.bic_encode(s, n1, lo, x - 1))
-        if (n2 := n - m - 1) > 0:
-            out.append(BICPostings.bic_encode(s[m + 1:], n2, x + 1, hi))
+        out = bitarray()
+        stack = []
+
+        # offset, n, low, high
+        stack.append((0, n, lo, hi))
+
+        while len(stack) > 0:
+            o, n, lo, hi = stack.pop()
+
+            m = n // 2
+            x = s[m + o]
+            out.extend(BICPostings.bit_representation(x - lo - m, hi - lo - n + 1))
+
+            # right side
+            if (n2 := n - m - 1) > 0:
+                stack.append((o + m + 1, n2, x + 1, hi))
+            
+            # left side
+            if (n1 := m) > 0:
+                stack.append((o, n1, lo, x - 1))
+
         return out
 
     @staticmethod
     def encode(postings_list, write_n=False):
         if len(postings_list) == 0:
-            return BitStream().tobytes()
-        out = BitStream()
+            return bitarray().tobytes()
+
+        out = bitarray()
+
         # write hi
         hi = postings_list[-1]
-        hi_bit_length = hi.bit_length()
-        hi_bit_length = max(hi_bit_length, 5) # sometimes we get 0
-        out.append(pack("uint:5", hi_bit_length))
-        out.append(pack("uint:b=x", b=hi_bit_length, x=hi))
+        hi_bit_length = max(hi.bit_length(), 1) # sometimes we get 0, default to 1
+        out.extend(int2ba(hi_bit_length, 5))
+        out.extend(int2ba(hi, hi_bit_length))
 
         n = len(postings_list)
-        if write_n:
-            # write n to our disk
-            # should also be there on the posting dict
-            n_bit_length = n.bit_length()
-            out.append(pack("uint:5", n_bit_length))
-            out.append(pack("uint:b=x", b=n_bit_length, x=n))
+
         # write BIC encoding
         if n > 1:
             encoded = BICPostings.bic_encode(postings_list[:-1], n - 1, 0, hi)
-            out.append(encoded)
+            out.extend(encoded)
         return out.tobytes()
 
     @staticmethod
-    def bic_decode(stream, n, lo, hi):
-        assert lo <= hi
-        m = n // 2
-        x = BICPostings.next_r_bits_to_int(stream, hi - lo - n + 1) + lo + m
-        out = []
-        if (n1 := m) > 0:
-            out = BICPostings.bic_decode(stream, n1, lo, x - 1)
-        out.append(x)
-        if (n2 := n - m - 1) > 0:
-            out = out + BICPostings.bic_decode(stream, n2, x + 1, hi)
+    def bic_decode(a, n, lo, hi):
+        out = [0] * n
+
+        stack = []
+        
+        stack.append((0, n, lo, hi))
+
+        pos = 0
+        while len(stack) > 0:
+            o, n, lo, hi = stack.pop()
+
+            m = n // 2
+            r = hi - lo - n + 1
+            l = len(int2ba(r))
+            x = ba2int(a[pos: pos + l]) + lo + m
+
+            out[m + o] = x
+            pos += l
+
+            if (n2 := n - m - 1) > 0:
+                stack.append((o + m + 1, n2, x + 1, hi))
+
+            if (n1 := m) > 0:
+                stack.append((o, n1, lo, x - 1))
+
         return out
 
     @staticmethod
     def decode(encoded_bytestream, n):
         if n == 0:
             return []
-        stream = BitStream(encoded_bytestream)
+        a = bitarray()
+        stream = a.frombytes(encoded_bytestream)
         # get hi
-        hi_bit_length = stream.read("uint:5")
-        hi = stream.read(f"uint:{hi_bit_length}")
-        if n is None:
-            # get n from the bytes
-            n_bit_length = stream.read("uint:5")
-            n = stream.read(f"uint:{n_bit_length}")
+        hi_bit_length = ba2int(a[:5])
+        hi = ba2int(a[5:5 + hi_bit_length])
         decoded = []
         if n > 1:
-            decoded = BICPostings.bic_decode(stream, n - 1, 0, hi)
+            decoded = BICPostings.bic_decode(a[5 + hi_bit_length:], n - 1, 0, hi)
         decoded.append(hi)
         return decoded
 
